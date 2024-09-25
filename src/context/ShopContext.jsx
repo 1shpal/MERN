38477186f9167f9c60a2b88bbd329da972bee278
1shpal <95474218+1shpal@users.noreply.github.com>
2024-09-25 22:05:02 +0530

    first commit

---
+++ src/context/ShopContext.jsx
@@ -0,0 +1,85 @@
+import { createContext, useEffect, useState } from "react";
+import {products } from '../assets/frontend_assets/assets'
+import { toast } from "react-toastify";
+
+export const ShopContext = createContext()
+const ShopContextProvider = (props)=>{
+    const currency = '₹'
+    const delivery_fee = 40
+    const  [search , setSearch] =useState('')
+    const [showSearch , setShowSearch] = useState(false)
+    const [cartItems , setCartItems] = useState({})
+    const addToCart= async (itemId , size)=>{
+        if(!size){
+            toast.error('Please select a size')
+            return
+        }
+             let cartData = structuredClone(cartItems)
+             if (cartData[itemId]){
+                if (cartData[itemId][size]){
+                    cartData[itemId][size] += 1
+                 }else{
+                    cartData[itemId][size] = 1 
+                 }
+             }else{
+                    cartData[itemId] ={}
+                    cartData[itemId][size] = 1;
+             }
+             setCartItems(cartData)
+            
+    }
+
+    const getCartCount = ()=>{
+        console.log("cart count" ,cartItems);
+        
+         let totalCout =0 ;
+         for (const items in cartItems) {
+            console.log(items);
+            
+            for (const item in cartItems[items]) {
+                console.log(item);
+                try {
+                    console.log("kuhkjhgkjh"  , cartItems[items][item]);
+                    
+                    if(cartItems[items][item] > 0 ){
+                     totalCout += cartItems[items][item];
+                    }
+                } catch (error) {
+                    console.log(error);
+                    
+                }
+                    
+                }
+            }
+          
+            console.log("total count" ,totalCout);
+         return totalCout
+    }
+    // useEffect(()=>{
+    //     console.log("catr called")
+    //     console.log(cartItems);
+        
+    // },[cartItems])
+    const updateQuantity= async (itemId , size , quantity)=>{
+            let cartData = structuredClone(cartItems)
+            cartData[itemId][size]=quantity
+            setCartItems(cartData)
+
+    }
+    const value ={
+            products,
+            currency,
+            delivery_fee,
+            search,
+            setSearch,
+            showSearch,
+            setShowSearch,
+            cartItems,addToCart,getCartCount,updateQuantity
+    }
+    return (
+        <ShopContext.Provider value={value}> 
+        {props.children}
+         </ShopContext.Provider>
+    )
+}
+export default ShopContextProvider
