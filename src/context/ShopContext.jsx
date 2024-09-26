70b6d4090a3d5282f53206756d2d1ff0bc5b4405
Vansh-CM <vansh@daytion.com>
2024-09-26 16:28:36 +0530

    complete frontend

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import {products } from '../assets/frontend_assets/assets'
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext()
 const ShopContextProvider = (props)=>{
@@ -9,6 +10,7 @@ const ShopContextProvider = (props)=>{
     const  [search , setSearch] =useState('')
     const [showSearch , setShowSearch] = useState(false)
     const [cartItems , setCartItems] = useState({})
+    const navigate = useNavigate()
     const addToCart= async (itemId , size)=>{
         if(!size){
             toast.error('Please select a size')
@@ -66,6 +68,24 @@ const ShopContextProvider = (props)=>{
             setCartItems(cartData)
 
     }
+
+    const getCartAmount = ()=>{
+        let totalAmount = 0 ;
+        for (const items in cartItems) {
+            let itemInfo = products.find((product)=> product._id === items)
+            for (const item in cartItems[items]) {
+               try {
+                if(cartItems[items][item] > 0){
+                    totalAmount += itemInfo.price * cartItems[items][item]
+                }
+               } catch (error) {
+                console.log(error);
+                
+               }
+            }
+        }
+        return totalAmount;
+    }
     const value ={
             products,
             currency,
@@ -74,7 +94,7 @@ const ShopContextProvider = (props)=>{
             setSearch,
             showSearch,
             setShowSearch,
-            cartItems,addToCart,getCartCount,updateQuantity
+            cartItems,addToCart,getCartCount,updateQuantity,getCartAmount,navigate
     }
     return (
         <ShopContext.Provider value={value}> 
